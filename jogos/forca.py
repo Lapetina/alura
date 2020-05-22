3b17a38e3a4c0872244c1112de5732d568833625
Lapetina <victor.lrs95@gmail.com>
2020-05-22 12:30:56 -0300

    retafatorando jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -17,37 +17,49 @@ def palavra_sorteada():
 def inicia_letras_acertadas(palavra):
     return ["_" for letra in palavra]
 
+
+def pede_chute():
+    chute = input("Qual letra? ").strip().upper()
+    return chute
+
+
+def chute_correto(chute, palavra_secreta, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def mensagem_resultado(resultado):
+    if resultado:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+
+
 def jogar():
     imprime_mensagem_abertura()
     palavra_secreta = palavra_sorteada()
     letras_acertadas = inicia_letras_acertadas(palavra_secreta)
     erros = 0
-    print("Dica: o tamanho da palavra é {}.".format(len(palavra_secreta)))
     print(letras_acertadas)
+    print("Dica: A palavra possui {} letras.".format(len(palavra_secreta)))
 
     while True:
-        chute = input("Qual letra? ").strip().upper()
+        chute = pede_chute()
         if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            chute_correto(chute, palavra_secreta, letras_acertadas)
         else:
             erros += 1
             print("Restam {} tentativas".format(len(palavra_secreta)-erros))
+        print(letras_acertadas)
         if erros == 6:
+            mensagem_resultado(False)
             break
         if "_" not in letras_acertadas:
+            mensagem_resultado(True)
             break
-        print(letras_acertadas)
-
-    if "_" not in letras_acertadas:
-        print(letras_acertadas)
-        print("Você ganhou!")
-    else:
-        print("Você perdeu!")
-    print("Fim do jogo")
 
 
 if __name__ == "__main__":
